770005c62463bfc55db67c3a1ba55a5011256b7a
Angel Mateu Paris <44613791+xSoraSS@users.noreply.github.com>
2020-03-02 12:53:27 +0100

    Update HashTable.java

--- src/ex1/HashTable.java
+++ src/ex1/HashTable.java
@@ -10,17 +10,19 @@ public class HashTable {
     private int size = 0;
     private HashEntry[] entries = new HashEntry[INITIAL_SIZE];
 
-    public int size(){
+    public int size() {
         /**
          * Siempre que haya una modificación, que no sean colisiones o sobreescriptura, este valor deberá variar.
+         *
          * @param size
          */
         return this.size;
     }
 
-    public int realSize(){
+    public int realSize() {
         /**
          * En cualquier caso de modificación este tamaño no debe aumentar.
+         *
          * @param INITIAL_SIZE Tamaño Real.
          */
         return this.INITIAL_SIZE;
@@ -30,10 +32,11 @@ public class HashTable {
         int hash = getHash(key);
         HashEntry hashEntry = new HashEntry(key, value);
 
-        if(entries[hash] == null) {
+        if (entries[hash] == null) {
             entries[hash] = hashEntry;
             /**
-             * ERROR: A continuación se ha añadido el size++ dentro del metodo Put para que cada vez que se inserte una nueva entrada está se sume al tamaño.
+             * ERROR: A continuación se ha añadido el size++ dentro del metodo Put para que cada vez que se inserte una nueva entrada esta se sume al tamaño.
+             *
              * @param size Almacena el tamaño actual de la hashtable.
              */
             size++;
@@ -68,10 +71,10 @@ public class HashTable {
      */
     public String get(String key) {
         int hash = getHash(key);
-        if(entries[hash] != null) {
+        if (entries[hash] != null) {
             HashEntry temp = entries[hash];
 
-            while( !temp.key.equals(key)) {
+            while (!temp.key.equals(key)) {
                 temp = temp.next;
             }
             return temp.value;
@@ -82,22 +85,40 @@ public class HashTable {
 
     public void drop(String key) {
         int hash = getHash(key);
-        if(entries[hash] != null) {
+        if (entries[hash] != null) {
 
             HashEntry temp = entries[hash];
-            while (!temp.key.equals(key))
+            while (!temp.key.equals(key)) {
                 temp = temp.next;
+            }
 
-            if(temp.prev == null) {
+            System.out.println("\nPrev **************** " + temp.prev);
+            System.out.println("Actu **************** " + temp);
+            System.out.println("Next **************** " + temp.next);
+
+            if (temp.prev == null) {
                 /**
-                 * ERROR: Se ha modificado el siguiente codigo para reemplazar la primera clave-valor dentro de la hashtable.
+                 * ERROR: A continuación se ha realizado la siguiente modificación para que al eliminar el primer elemento no elimine el resto que puedan colisionar.
                  */
-                entries[hash] = temp.next; //esborrar element únic (no col·lissió)
-            }else{
-                if(temp.next != null) {
-                    temp.next.prev = temp.next; //esborrem temp, per tant actualitzem l'anterior al següent
-                }else {
-                    temp.prev.next = temp.next; //esborrem temp, per tant actualitzem el següent de l'anterior
+                entries[hash] = temp.next; // esborrar element únic (no col·lissió)
+                if (temp.next == null) {
+                    size--;
+                }
+            } else {
+                if (temp.next != null) {
+                    /**
+                     * ERROR: La siguiente comrobación ha sido añadida para poder eliminar el elemento localizado entre principio y final sin eliminar el resto.
+                     */
+                    if (temp.key.equals(key)) {
+                        entries[hash] = temp.next;
+                    } else {
+                        temp.next.prev = temp.prev; // esborrem temp, per tant actualitzem l'anterior al següent
+                    }
+                } else if (temp.next == null) {
+                    temp.prev.next = temp.next; // esborrem temp, per tant actualitzem el següent de l'anterior
+                    if (entries[hash].next == null) {
+                        size--;
+                    }
                 }
             }
         }
@@ -135,17 +156,14 @@ public class HashTable {
         int bucket = 0;
         StringBuilder hashTableStr = new StringBuilder();
         for (HashEntry entry : entries) {
-            if(entry == null) {
+            if (entry == null) {
                 bucket++;
                 continue;
             }
-            hashTableStr.append("\n bucket[")
-                    .append(bucket)
-                    .append("] = ")
-                    .append(entry.toString());
+            hashTableStr.append("\n bucket[").append(bucket).append("] = ").append(entry.toString());
             bucket++;
             HashEntry temp = entry.next;
-            while(temp != null) {
+            while (temp != null) {
                 hashTableStr.append(" -> ");
                 hashTableStr.append(temp.toString());
                 temp = temp.next;
@@ -158,64 +176,60 @@ public class HashTable {
         return getCollisionsForKey(key, 1);
     }
 
-    public ArrayList<String> getCollisionsForKey(String key, int quantity){
+    public ArrayList<String> getCollisionsForKey(String key, int quantity) {
         /*
-          Main idea:
-          alphabet = {0, 1, 2}
+         * Main idea: alphabet = {0, 1, 2} Step 1: "000" Step 2: "001" Step 3: "002" Step 4: "010" Step 5: "011" ... Step N: "222" All those keys will be hashed and
+         * checking if collides with the given one.
+         */
 
-          Step 1: "000"
-          Step 2: "001"
-          Step 3: "002"
-          Step 4: "010"
-          Step 5: "011"
-           ...
-          Step N: "222"
-
-          All those keys will be hashed and checking if collides with the given one.
-        * */
-
-        final char[] alphabet = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+        final char[] alphabet = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
         ArrayList<Integer> newKey = new ArrayList();
         ArrayList<String> foundKeys = new ArrayList();
 
         newKey.add(0);
         int collision = getHash(key);
-        int current = newKey.size() -1;
+        int current = newKey.size() - 1;
 
-        while (foundKeys.size() < quantity){
-            //building current key
+        while (foundKeys.size() < quantity) {
+            // building current key
             String currentKey = "";
-            for(int i = 0; i < newKey.size(); i++)
+            for (int i = 0; i < newKey.size(); i++) {
                 currentKey += alphabet[newKey.get(i)];
+            }
 
-            if(!currentKey.equals(key) && getHash(currentKey) == collision)
+            if (!currentKey.equals(key) && getHash(currentKey) == collision) {
                 foundKeys.add(currentKey);
+            }
 
-            //increasing the current alphabet key
-            newKey.set(current, newKey.get(current)+1);
+            // increasing the current alphabet key
+            newKey.set(current, newKey.get(current) + 1);
 
-            //overflow over the alphabet on current!
-            if(newKey.get(current) == alphabet.length){
+            // overflow over the alphabet on current!
+            if (newKey.get(current) == alphabet.length) {
                 int previous = current;
-                do{
-                    //increasing the previous to current alphabet key
+                do {
+                    // increasing the previous to current alphabet key
                     previous--;
-                    if(previous >= 0)  newKey.set(previous, newKey.get(previous) + 1);
-                }
-                while (previous >= 0 && newKey.get(previous) == alphabet.length);
+                    if (previous >= 0) {
+                        newKey.set(previous, newKey.get(previous) + 1);
+                    }
+                } while (previous >= 0 && newKey.get(previous) == alphabet.length);
 
-                //cleaning
-                for(int i = previous + 1; i < newKey.size(); i++)
+                // cleaning
+                for (int i = previous + 1; i < newKey.size(); i++) {
                     newKey.set(i, 0);
+                }
 
-                //increasing size on underflow over the key size
-                if(previous < 0) newKey.add(0);
+                // increasing size on underflow over the key size
+                if (previous < 0) {
+                    newKey.add(0);
+                }
 
-                current = newKey.size() -1;
+                current = newKey.size() - 1;
             }
         }
 
-        return  foundKeys;
+        return foundKeys;
     }
 
     public static void main(String[] args) {
@@ -223,7 +237,7 @@ public class HashTable {
 
         System.out.println(hashTable.getCollisionsForKey("3", 9));
         // Put some key values.
-        for(int i=0; i<30; i++) {
+        for (int i = 0; i < 30; i++) {
             final String key = String.valueOf(i);
             hashTable.put(key, key);
         }
@@ -231,7 +245,7 @@ public class HashTable {
         // Print the HashTable structure
         log("****   HashTable  ***");
         log(hashTable.toString());
-        log("\nValue for key(20) : " + hashTable.get("20") );
+        log("\nValue for key(20) : " + hashTable.get("20"));
     }
 
     private static void log(String msg) {
